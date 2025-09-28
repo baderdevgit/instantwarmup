49d189757e32963bdcada22663f3183e3a33f8c1
Bader <bader.kharbat@gmail.com>
2025-09-28 17:29:15 -0400

    Corrected Relay State

--- src/main.cpp
+++ src/main.cpp
@@ -18,14 +18,16 @@ int middle_scoop_pin       = 32;
 int monster_tilt_pin       = 13;
 int monster_lid_pin        = 12;
 
-int left_bicept_relay  = 4;
-int right_bicept_relay = 5;
-int left_tricept_relay = 16;
-int right_tricept_relay= 17;
-int left_front_delt_relay    = 14;
-int right_front_delt_relay   = 15;
-int left_side_delt_relay    = 21;
-int right_side_delt_relay   = 22;
+int left_bicept_relay  = 23;
+int right_bicept_relay = 22;
+int left_tricept_relay = 21;
+int right_tricept_relay= 19;
+int left_front_delt_relay  = 18;
+int right_front_delt_relay = 5;
+int left_forearm_relay    = 4;
+int right_forearm_relay   = 15;
+
+int relayArray[] = {23, 22, 21, 19, 18, 5, 4, 15};
 
 // Servo objects
 Servo bot_scoop_sweep_servo;
@@ -71,17 +73,17 @@ void setup() {
   pinMode(right_tricept_relay, OUTPUT);
   pinMode(left_front_delt_relay, OUTPUT);
   pinMode(right_front_delt_relay, OUTPUT);
-  pinMode(left_side_delt_relay, OUTPUT);
-  pinMode(right_side_delt_relay, OUTPUT);
+  pinMode(left_forearm_relay, OUTPUT);
+  pinMode(right_forearm_relay, OUTPUT);
 
-  digitalWrite(left_bicept_relay, LOW);
-  digitalWrite(right_bicept_relay, LOW);
-  digitalWrite(left_tricept_relay, LOW);
-  digitalWrite(right_tricept_relay, LOW);
-  digitalWrite(left_front_delt_relay, LOW);
-  digitalWrite(right_front_delt_relay, LOW);
-  digitalWrite(left_side_delt_relay, LOW);
-  digitalWrite(right_side_delt_relay, LOW);
+  digitalWrite(left_bicept_relay, HIGH);
+  digitalWrite(right_bicept_relay, HIGH);
+  digitalWrite(left_tricept_relay, HIGH);
+  digitalWrite(right_tricept_relay, HIGH);
+  digitalWrite(left_front_delt_relay, HIGH);
+  digitalWrite(right_front_delt_relay, HIGH);
+  digitalWrite(left_forearm_relay, HIGH);
+  digitalWrite(right_forearm_relay, HIGH);
 }
 
 void loop() {
@@ -91,10 +93,19 @@ void loop() {
 
     if (c == 'd') {
       Serial.println("D key pressed");
-      SupplementSequence();
+
+      // SupplementSequence();
+      MuscleContractionSequence();
+      delay(200);
     }
   }
 
+}
+
+void MuscleContractionSequence() {
+  CurlArm(right);
+  delay(500);
+  StraightenArm(right);
 }
 
 void SupplementSequence() {
@@ -171,38 +182,48 @@ enum Side {
   right
 };
 
+void CurlArm(Side side) {
+  ToggleBicept(side, true);
+  ToggleTricept(side, false);
+}
+
+void StraightenArm(Side side) {
+  ToggleBicept(side, false);
+  ToggleTricept(side, true);
+}
+
 void ToggleBicept(Side side, bool on) {
   if (side == left) {
-      digitalWrite(left_bicept_relay, on ? HIGH : LOW);
+      digitalWrite(left_bicept_relay, on ? LOW : HIGH);
   } 
   else {
-      digitalWrite(right_bicept_relay, on ? HIGH : LOW);
+      digitalWrite(right_bicept_relay, on ? LOW : HIGH);
   }
 }
 
 void ToggleTricept(Side side, bool on) {
   if (side == left) {
-      digitalWrite(left_tricept_relay, on ? HIGH : LOW);
+      digitalWrite(left_tricept_relay, on ? LOW : HIGH);
   } 
   else {
-      digitalWrite(right_tricept_relay, on ? HIGH : LOW);
+      digitalWrite(right_tricept_relay, on ? LOW : HIGH);
   }
 }
 
 void ToggleSideDelt(Side side, bool on) {
   if (side == left) {
-      digitalWrite(left_side_delt_relay, on ? HIGH : LOW);
+      digitalWrite(left_forearm_relay, on ? LOW : HIGH);
   } 
   else {
-      digitalWrite(right_side_delt_relay, on ? HIGH : LOW);
+      digitalWrite(right_forearm_relay, on ? LOW : HIGH);
   }
 }
 
 void ToggleFrontDelt(Side side, bool on) {
   if (side == left) {
-      digitalWrite(left_front_delt_relay, on ? HIGH : LOW);
+      digitalWrite(left_front_delt_relay, on ? LOW : HIGH);
   } 
   else {
-      digitalWrite(right_front_delt_relay, on ? HIGH : LOW);
+      digitalWrite(right_front_delt_relay, on ? LOW : HIGH);
   }
 }
